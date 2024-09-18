63b13d1baee13f7a2a3ce1b9e00befefc59f062e
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-09-18 14:53:16 +0000

    a

--- PP1_3.py
+++ PP1_3.py
@@ -3,14 +3,33 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
+ print(" * * * * * *")
+ print("* * * * * *")   
+ print(" * * * * * *")
+ print("* * * * * *")
+ print(" * * * * * *")
+ print("* * * * * *")
+
+ print("* * * * *")
+ print("*       *")
+ print("*       *")
+ print("*       *")
+ print("* * * * *")
+
+ print("* * * * * *")
+ print(" *       *")
+ print("  *     *")
+ print("   *   *")
+ print("    * *")
+ print("     *")
 
 def q2():
-  #Write Assignment code here
-  
+  #Wrte Assignment code here
+  print("Hello",end="")
+  print("World")
 def q3():
   #Write Assignment code here
-  
-
+ print('"Man"\n"Board"\n"Man Overboard"') 
 
 #Do not alter anything after this line
 q1()
